1d4a0d50808ffa6140bb1ea22af944a0ba4080ce
pedro <pedro.paz13@hotmail.com>
2023-04-26 10:55:04 -0300

    subindo arquivos de configuração

--- src/ClasseDescricao.java
+++ src/ClasseDescricao.java
@@ -11,10 +11,54 @@ public class ClasseDescricao {
 	 * 
 	 */
 	
-		public static void main(String[] args) {
-			System.out.println("Oi, fui executado pelo terminal");
-			
-		}
+	/**
+	 * 
+	 *	Observe que nosso projeto Java criado por uma IDE, terá uma pasta chamada 
+	 *bin. É nesta pasta que ficarão os arquivos .class, o nosso bytecode
+	 * 
+	 */
+	
+		/**
+		 * 
+		 * Mesmo usando uma IDE, nós sempre precisaremos identificar aonde se 
+		 * encontram as classes do nosso projeto, no meu caso está em: 
+		 * C:\estudos\dio-trilha-java-basico\java-terminal.
+		 * 
+		 */
+		
+			//TERMINAL
+			/**
+			 * 
+			 * Vamos ilustrar como executar uma classe, depois de compilada, sem precisar usar a IDE.
+			 *	Abra o MS-DOS ou Power Shell;
+			 *	Localize o diretório do seu projeto: cd C:\estudos\dio-trilha-java-basico\java-terminal;
+			 *	Acesse a pasta **** bin: ** cd bin**;
+			 *	Agora digite o comando:**java MinhaClasse ** (nome da sua classe sem a extensão .class).
+			 * 
+			 */
+	
+			//ARGUMENTOS
+		
+			/**
+			 *
+			 * 	Quando executamos uma classe, que contenha o método main, 
+			 * 	o mesmo permite que passemos um array [] de argumentos, 
+			 * 	do tipo String. Logo, podemos após a definição da classe a ser 
+			 * 	executada, informar estes parâmetros, exemplo:
+			 *	java MinhaClasse agumentoUm argumentoDois
+    		 *
+			 * 
+			 */
+	   public static void main(String[] args) {
+	        //os argumentos começam com indice 0
+	        String nome = args [0];
+	        String sobrenome = args [1];
+	        int idade = Integer.valueOf(args[2]); //vamos falar sobre Wrappers
+	        double altura = Double.valueOf(args[3]);
 
+	        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+	        System.out.println("Tenho " + idade + " anos ");
+	        System.out.println("Minha altura é " + altura + "cm ");
+	    }
 }
 
