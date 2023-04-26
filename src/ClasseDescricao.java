5f278a9b65e0c9848cb2851f36c9c99e50ea64ec
pedro <pedro.paz13@hotmail.com>
2023-04-26 10:48:04 -0300

    subindo pasta principal do projeto

---
+++ src/ClasseDescricao.java
@@ -0,0 +1,20 @@
+
+public class ClasseDescricao {
+	
+	//TERMINAL E ARGUMENTOS
+	/**
+	 * 
+	 *  Nem sempre executamos nosso programa Java pela IDE, já pensou, nossos clientes tendo que instalar o Eclipse ou VsCode para rodar o sistema em sua empresa ?
+	 *  Com a JVM devidamente configurada, nós podemos criar um executável do nosso programa e disponibilizar o instalador para qualquer sistema operacional.
+	 *  No nosso caso, iremos aprender como executar um programa Java via terminal, como MS - DOS ou terminal do VsCode.
+	 *  Vamos criar uma classe chamada MinhaClasse.java com o código abaixo:
+	 * 
+	 */
+	
+		public static void main(String[] args) {
+			System.out.println("Oi, fui executado pelo terminal");
+			
+		}
+
+}
+
